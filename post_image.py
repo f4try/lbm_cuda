831dd072845fa398c18859f628630ba94ca26e9f
zz <zongzh.hd@163.com>
2021-05-14 11:05:54 +0800

    xct

--- post_image.py
+++ post_image.py
@@ -3,6 +3,7 @@ import numpy as np
 img_ketton = Image.open("ketton.tif").convert("1")
 mask_ketton=np.where(np.array(img_ketton),0,1)
 mask_ketton=mask_ketton.T
+# mask_ketton=np.zeros(shape=(1024,512))
 print(mask_ketton)
 np.savetxt("ketton.txt",mask_ketton) 
 # img_ketton.show()
@@ -19,9 +20,23 @@ np.savetxt("ketton.txt",mask_ketton)
 # np.savetxt("berea.txt",mask_berea) 
 # # img_berea.show()
 
-img_fc = Image.open("fc.bmp").convert("L")
-mask_fc=np.where(np.array(img_fc),0,1)
-# mask_fc=mask_fc.T
-print(mask_fc.shape)
-np.savetxt("fc.txt",mask_fc) 
-# Image.fromarray(img_fc).show()
+# img_fc = Image.open("fc.bmp").convert("L")
+# mask_fc=np.where(np.array(img_fc),0,1)
+# # mask_fc=mask_fc.T
+# print(mask_fc.shape)
+# np.savetxt("fc.txt",mask_fc) 
+# # Image.fromarray(img_fc).show()
+
+img_xct_l = Image.open("xct_0414_left.tif")
+mask_xct_l=np.where(np.array(img_xct_l)>150,0,1)
+# mask_xct_l=np.zeros_like(mask_xct_l)
+print(mask_xct_l.shape)
+print(mask_xct_l)
+np.savetxt("xct_0414_left.txt",mask_xct_l) 
+
+img_xct_r = Image.open("xct_0414_right.tif")
+mask_xct_r=np.where(np.array(img_xct_r)>150,0,1)
+# mask_xct_r=np.zeros_like(mask_xct_r)
+print(mask_xct_r.shape)
+print(mask_xct_r)
+np.savetxt("xct_0414_right.txt",mask_xct_r) 
